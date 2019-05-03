b82b8560dd988f9a01381f76ed767cc5695a6e31
Danny Avery <danny.w.avery@gmail.com>
2019-05-03 10:11:44 -0600

    update welcome.blade.php + .gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Queues Redis
                 </div>
 
                 <div class="links">
